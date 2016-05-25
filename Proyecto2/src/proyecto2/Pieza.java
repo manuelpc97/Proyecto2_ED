8c61b3997ca7c6717bf91409bcef270cbb6f2cfa
joseluisfiallosguerrero1 <jose.fiallos@unitec.edu>
2016-05-25 14:58:38 -0600

    Starting GUI

---
+++ Proyecto2/src/proyecto2/Pieza.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto2;
+
+/**
+ *
+ * @author jose
+ */
+public class Pieza {
+    boolean Pieza;
+
+    public Pieza() {
+    }
+
+    public Pieza(boolean Pieza) {
+        this.Pieza = Pieza;
+    }
+    
+}
